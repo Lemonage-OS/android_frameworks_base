2eb1fcdb41c54ab24e90c71f25ce7677c09f8cbd
Ido Ben-Hur <idoybh2@gmail.com>
2025-09-28 17:56:13 +0200

    base: Allow toggling floating rotation button [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4827,6 +4827,12 @@ public final class Settings {
         public static final String DATA_DISABLED_ICON = "data_disabled_icon";
 
         /**
+         * Whether to show floating rotation button
+         * @hide
+         */
+        public static final String ENABLE_FLOATING_ROTATION_BUTTON = "enable_floating_rotation_button";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
